4513da58dfd7bed0af3441715f493e8d043da864
Ubuntu <ubuntu@ubuntu.lan>
2021-02-09 17:59:51 +0000

    added ui and prompt user to enter a string

---
+++ src/ui.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 50
+
+/* interface to take user input*/
+int main() {
+	char str[MAX]
+
+	printf("Enter a string to get started or enter q to exit \n");
+	
+}
+
