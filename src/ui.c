364b477a5afcaed36f3cb7957adc262b21efd63b
Ubuntu <ubuntu@ubuntu.lan>
2021-02-09 18:20:30 +0000

    users can enter input and have it printed out

--- src/ui.c
+++ src/ui.c
@@ -5,9 +5,19 @@
 
 /* interface to take user input*/
 int main() {
-	char str[MAX]
+
+	char str[MAX];
 
 	printf("Enter a string to get started or enter q to exit \n");
 	
+	/* checks if input is q. if true quit else continue getting input */
+	while(*str != 'q') {
+		printf("> ");
+		fgets(str, MAX, stdin);
+		printf("You entered: %s\n", str);
+	}
+	
+	printf("System exited successfully!\n\n");
+	return 0;
 }
 
